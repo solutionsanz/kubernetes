6de5b29c54479fa43a7ffe9e66435acd3638d1af
SolutionsANZ <solutionsanz2@gmail.com>
2018-04-20 13:40:39 +1000

    Adding HellowWorldApp

---
+++ helloWorldApp/Dockerfile
@@ -0,0 +1,6 @@
+FROM node:9.11.1
+WORKDIR /myProject
+ADD . /myProject
+RUN npm install
+EXPOSE 3000
+CMD npm start
---
+++ helloWorldApp/app.js
@@ -0,0 +1,18 @@
+var config = require('./config');
+var express = require('express');
+
+// Create an Express web app
+var app = express();
+
+//Create API root Endpoint to listen to
+app.get('/', function (req, res) {
+
+    // Returning Hello World Message:
+    res.send('Hello World from a NodeJS Application');
+});
+
+// Create an HTTP server to run our application
+var server = app.listen(config.PORT, function () {
+
+    console.log('Hello World Application running on port ' + config.PORT + "!!!");
+});
