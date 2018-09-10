e6351d02de7cb9fd2818c3a9019ae12e5f521be1
SolutionsANZ <solutionsanz2@gmail.com>
2018-09-10 19:21:42 +1000

    Adding Vagrantfile for ease of use

---
+++ helloWorldApp/bootstrap.sh
@@ -0,0 +1,25 @@
+    echo "##########################################################################"
+    echo "###################### Updating packages ##############################"
+
+    sudo apt-get update
+
+    echo "##########################################################################"    
+    echo "###################### Installing Git ##############################"
+
+    sudo apt-get install git -y
+   
+    echo "##########################################################################"
+    echo "############### Installing NodeJS on an Ubuntu Machine ###############"
+
+    sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+    sudo apt-get install nodejs -y
+
+    echo "##########################################################################"
+    echo "############# Installing and configuring Docker for Dev #######################"
+
+    sudo apt-get install docker.io -y
+    sudo usermod -G docker ubuntu    
+    docker --version
+
+    
